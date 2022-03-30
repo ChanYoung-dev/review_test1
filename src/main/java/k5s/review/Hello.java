69c776d787c8f299ba2168fe732979bca5b5a3ed
emrhssla <emrhssla@gmail.com>
2022-03-30 15:30:09 +0900

    Initialized project

---
+++ src/main/java/k5s/review/Hello.java
@@ -0,0 +1,10 @@
+package k5s.review;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Hello {
+    private String data;
+}
---
+++ src/main/java/k5s/review/ReviewApplication.java
@@ -0,0 +1,19 @@
+package k5s.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewApplication {
+
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println("data = " + data);
+
+
+		SpringApplication.run(ReviewApplication.class, args);
+	}
+
+}
