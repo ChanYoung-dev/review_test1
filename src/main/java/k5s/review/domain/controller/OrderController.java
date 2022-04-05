70d8c9d0698efe4dcab847c1e47f31a4cd38afe0
emrhssla <emrhssla@gmail.com>
2022-04-05 15:41:51 +0900

    상품 검색 및 리스트

--- src/main/java/k5s/review/domain/controller/OrderController.java
+++ src/main/java/k5s/review/domain/controller/OrderController.java
@@ -1,6 +1,8 @@
 package k5s.review.domain.controller;
 
 import k5s.review.domain.Member;
+import k5s.review.domain.Order;
+import k5s.review.domain.OrderSearch;
 import k5s.review.domain.item.Item;
 import k5s.review.service.ItemService;
 import k5s.review.service.MemberService;
@@ -8,9 +10,7 @@ import k5s.review.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,6 +35,19 @@ public class OrderController {
     public String order(@RequestParam("memberId") Long memberId,
                         @RequestParam("itemId") Long itemId, @RequestParam("count") int count) {
         orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping(value = "/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        return "order/orderList";
+    }
+
+    @PostMapping(value = "/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
         return "redirect:/orders";
     }
 
